f1c1f91841973ca2ee55d9b348ffb692b8b88991
John-Alvin Ambalong <119690431+jambalong@users.noreply.github.com>
2025-12-14 19:37:32 -0800

    feat(service): add call method and input validation for level and ascension rank

--- app/services/weapon_ascension_planner.rb
+++ app/services/weapon_ascension_planner.rb
@@ -13,4 +13,31 @@ class WeaponAscensionPlanner < ApplicationService
 
     @materials_totals = Hash.new(0)
   end
+
+  def call
+    validate_inputs!
+  end
+
+  private
+
+  def validate_inputs!
+    errors = []
+
+    # this checks for a zero-change plan
+    if @current_level == @target_level &&
+       @current_ascension_rank == @target_ascension_rank
+      errors << "At least one target value must be different from the corresponding current value to generate a plan."
+    end
+
+    # this checks for impossible downgrades (level and ascension rank)
+    if @target_level < @current_level
+      errors << "Target Level (#{@target_level}) cannot be less than Current Level (#{@current_level})."
+    end
+
+    if @target_ascension_rank < @current_ascension_rank
+      errors << "Target ascension rank value must be greater than or equal to current ascension rank value."
+    end
+
+    raise "Input Validation Error: #{errors.join('; ')}" unless errors.empty?
+  end
 end
