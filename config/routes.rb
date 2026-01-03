a0eca2f30504b2ff7555ee7393e33d7d21e16df6
John-Alvin Ambalong <119690431+jambalong@users.noreply.github.com>
2026-01-03 00:56:44 -0800

    feat(pages): add improved header, about, home, and dashboard pages

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,9 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "pages#home"
 
+  get "about", to: "pages#about"
+  get "dashboard", to: "pages#dashboard"
+
   resources :plans, only: [ :index, :new, :create, :destroy ], path: :planner do
     member do
       get :confirm_delete
