3ad7682f08aff4b6ea74a58390dd3c0ae640ed1b
John-Alvin Ambalong <119690431+jambalong@users.noreply.github.com>
2026-02-15 15:44:41 -0800

    feat: render material icons in every relevant views

--- app/models/inventory_item.rb
+++ app/models/inventory_item.rb
@@ -7,7 +7,7 @@ class InventoryItem < ApplicationRecord
   validates :user_id, uniqueness: { scope: :material_id, message: "can only have one inventory item per material" }
 
   # Allows inventory_item.name instead of material.name
-  delegate :name, :rarity, :material_type, :category, to: :material
+  delegate :name, :rarity, :material_type, :category, :image_url, to: :material
 
   scope :search_by_name, ->(query) { joins(:material).where("materials.name ILIKE ?", "%#{query}%") }
   scope :by_category, ->(category) { joins(:material).where(materials: { category: category }) }
