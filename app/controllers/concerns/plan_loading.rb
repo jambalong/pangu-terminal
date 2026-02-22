1ea327313a74b0635d22c1a2246d9be57313562e
John-Alvin Ambalong <119690431+jambalong@users.noreply.github.com>
2026-02-22 15:29:09 -0800

    refactor: add #includes(:subject) to load plans to avoid N+1 queries

--- app/controllers/concerns/plan_loading.rb
+++ app/controllers/concerns/plan_loading.rb
@@ -1,9 +1,9 @@
 module PlanLoading
   def load_current_plans
     plans = if user_signed_in?
-      current_user.plans
+      current_user.plans.includes(:subject)
     else
-      Plan.where(user_id: nil, guest_token: @guest_token)
+      Plan.where(user_id: nil, guest_token: @guest_token).includes(:subject)
     end
 
     plans.order(created_at: :asc)
