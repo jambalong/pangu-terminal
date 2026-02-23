205c0ff34acf98c4c5b3db0795293ce6b2bef1d3
John-Alvin Ambalong <119690431+jambalong@users.noreply.github.com>
2026-02-22 18:24:50 -0800

    fix: #includes eager loading is not compatible with polymorphism so use #preload instead

--- app/controllers/concerns/plan_loading.rb
+++ app/controllers/concerns/plan_loading.rb
@@ -1,9 +1,9 @@
 module PlanLoading
   def load_current_plans
     plans = if user_signed_in?
-      current_user.plans.includes(:subject)
+      current_user.plans.preload(:subject)
     else
-      Plan.where(user_id: nil, guest_token: @guest_token).includes(:subject)
+      Plan.where(user_id: nil, guest_token: @guest_token).preload(:subject)
     end
 
     plans.order(created_at: :asc)
