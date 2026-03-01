0b16000731ba66d05b9c284f3a0c0a70cc93b5a7
John-Alvin Ambalong <119690431+jambalong@users.noreply.github.com>
2026-03-01 11:22:16 -0800

    feat: add generate token to ApiKey model

--- app/models/api_key.rb
+++ app/models/api_key.rb
@@ -1,7 +1,14 @@
 class ApiKey < ApplicationRecord
   belongs_to :user
 
-  has_secure_token :token, length: 36
+  attr_reader :raw_token
 
   validates :name, presence: true
+
+  before_create :generate_token
+
+  def generate_token
+    @raw_token = "pt_#{SecureRandom.urlsafe_base64(32)}"
+    self.token = Digest::SHA256.hexdigest(@raw_token)
+  end
 end
