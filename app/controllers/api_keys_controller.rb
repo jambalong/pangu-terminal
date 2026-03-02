d039c5dedfbca88705747d566e49250af25ae866
John-Alvin Ambalong <119690431+jambalong@users.noreply.github.com>
2026-03-01 17:25:26 -0800

    feat: add API Key management UI and hashed token storage to db

---
+++ app/controllers/api_keys_controller.rb
@@ -0,0 +1,20 @@
+class ApiKeysController < ApplicationController
+  before_action :authenticate_user!
+
+  def create
+    @api_key = current_user.api_keys.build(name: params[:name])
+    if @api_key.save
+      flash[:api_token] = @api_key.raw_token
+      flash[:notice] = "API Key \"#{@api_key.name}\" generated successfully."
+    else
+      flash[:alert] = @api_key.errors.full_messages.to_sentence
+    end
+
+    redirect_to "#{edit_user_registration_path}#api-keys"
+  end
+
+  def destroy
+    current_user.api_keys.find(params[:id]).destroy
+    redirect_to "#{edit_user_registration_path}#api-keys"
+  end
+end
