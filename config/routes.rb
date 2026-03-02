d7af4db3f3bf45f4d0c6810782790278b9960c96
John-Alvin Ambalong <119690431+jambalong@users.noreply.github.com>
2026-03-01 19:56:30 -0800

    feat: add route to GET /api/v1/inventory endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -41,6 +41,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :plans, only: [ :index ]
+      resources :inventory, only: [ :index ]
     end
   end
 
