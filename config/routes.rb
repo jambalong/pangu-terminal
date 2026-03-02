c0009ae7be4d620a47326e7244fac0940d00976b
John-Alvin Ambalong <119690431+jambalong@users.noreply.github.com>
2026-03-01 21:52:14 -0800

    feat: add route to POST /api/v1/plans endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -40,7 +40,7 @@ Rails.application.routes.draw do
   # API routes
   namespace :api do
     namespace :v1 do
-      resources :plans, only: [ :index ]
+      resources :plans, only: [ :index, :create ]
       resources :inventory, only: [ :index ]
     end
   end
