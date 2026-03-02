8f85b57fc017d3aebc1c26d4faf1acdc3c728a49
John-Alvin Ambalong <119690431+jambalong@users.noreply.github.com>
2026-03-01 19:03:32 -0800

    feat: added a limit of 5 to api keys per user

--- app/models/api_key.rb
+++ app/models/api_key.rb
@@ -4,11 +4,20 @@ class ApiKey < ApplicationRecord
   attr_reader :raw_token
 
   validates :name, presence: true
+  validate :api_key_limit
 
   before_create :generate_token
 
   def generate_token
     @raw_token = "pt_#{SecureRandom.urlsafe_base64(32)}"
     self.token = Digest::SHA256.hexdigest(@raw_token)
+  end
+
+  private
+
+  def api_key_limit
+    if user.api_keys.count >= 5
+      errors.add(:base, "Maximum of 5 API Keys allowed")
+    end
   end
 end
