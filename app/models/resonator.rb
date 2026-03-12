c64daad3c721a1bad44b5d0ba4234dea833d8e6b
John Alvin Ambalong <119690431+jambalong@users.noreply.github.com>
2026-03-11 22:37:50 -0700

    feat: add model validations to Resonator and Weapon

--- app/models/resonator.rb
+++ app/models/resonator.rb
@@ -1,3 +1,7 @@
 class Resonator < ApplicationRecord
   has_many :plans, as: :subject, dependent: :destroy
+
+  validates :name, presence: true, uniqueness: true
+  validates :element, presence: true
+  validates :weapon_type, presence: true
 end
--- app/models/weapon.rb
+++ app/models/weapon.rb
@@ -1,3 +1,6 @@
 class Weapon < ApplicationRecord
   has_many :plans, as: :subject, dependent: :destroy
+
+  validates :name, presence: true, uniqueness: true
+  validates :weapon_type, presence: true
 end
