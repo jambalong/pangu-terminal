33422ce254f7311d6704226740287b04fc8372e2
John Alvin Ambalong <119690431+jambalong@users.noreply.github.com>
2026-03-22 19:11:27 -0700

    style: replace inline rarity class interpolation with helper methods

--- app/helpers/inventory_items_helper.rb
+++ app/helpers/inventory_items_helper.rb
@@ -5,4 +5,12 @@ module InventoryItemsHelper
     classes << "used-higher-rarity" if reconciliation&.dig(:used_higher_rarity)
     classes.join(" ")
   end
+
+  def rarity_border_class(rarity)
+    "rarity-border-#{rarity}"
+  end
+
+  def rarity_bg_class(rarity)
+    "rarity-bg-#{rarity}"
+  end
 end
