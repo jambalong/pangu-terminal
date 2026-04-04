423ca6cc6808d6cee4842a66621c50a571df2a16
John Alvin Ambalong <119690431+jambalong@users.noreply.github.com>
2026-04-04 13:28:52 -0700

    feat: update DropRate model - remove material association, add rarity validation

--- app/models/drop_rate.rb
+++ app/models/drop_rate.rb
@@ -1,8 +1,8 @@
 class DropRate < ApplicationRecord
-  belongs_to :material
   belongs_to :source
 
   validates :sol3_phase, presence: true, numericality: { only_integer: true, in: 1..8 }
+  validates :rarity, presence: true, numericality: { only_integer: true, in: 1..5 }
   validates :avg_quantity, presence: true, numericality: { greater_than: 0 }
-  validates :material_id, uniqueness: { scope: [ :source_id, :sol3_phase ] }
+  validates :source_id, uniqueness: { scope: [ :sol3_phase, :rarity ] }
 end
