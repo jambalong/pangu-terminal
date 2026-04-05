4a89bc3883bfc0e76f51c65615635480b1b1c423
John Alvin Ambalong <119690431+jambalong@users.noreply.github.com>
2026-04-04 20:43:00 -0700

    feat: add material_type validation and update uniqueness scope on DropRate model

--- app/models/drop_rate.rb
+++ app/models/drop_rate.rb
@@ -4,5 +4,5 @@ class DropRate < ApplicationRecord
   validates :sol3_phase, presence: true, numericality: { only_integer: true, in: 1..8 }
   validates :rarity, presence: true, numericality: { only_integer: true, in: 1..5 }
   validates :avg_quantity, presence: true, numericality: { greater_than: 0 }
-  validates :source_id, uniqueness: { scope: [ :sol3_phase, :rarity ] }
+  validates :source_id, uniqueness: { scope: [ :sol3_phase, :rarity, :material_type ] }
 end
