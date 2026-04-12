d39978f32eb06c1aa0ef783d56a76c2849c8f973
John Alvin Ambalong <119690431+jambalong@users.noreply.github.com>
2026-04-12 03:10:24 -0700

    feat: add SOL3 phase selector to dashboard cards

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,7 +23,7 @@ class ApplicationController < ActionController::Base
   end
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permite(:account_update, keys: [ :sol3_phase ])
+    devise_parameter_sanitizer.permit(:account_update, keys: [ :sol3_phase ])
   end
 
   private
