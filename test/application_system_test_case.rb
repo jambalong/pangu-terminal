35b176b46b54f83796c32b7eaa9fe44340d56695
John Alvin Ambalong <119690431+jambalong@users.noreply.github.com>
2026-04-27 00:02:07 -0700

    test: configure Cuprite with no-sandbox option for CI compatibility

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,6 +1,15 @@
 require "test_helper"
 require "capybara/cuprite"
 
+Capybara.register_driver(:cuprite) do |app|
+  Capybara::Cuprite::Driver.new(
+    app,
+    window_size: [ 1400, 1400 ],
+    browser_options: { "no-sandbox": nil },
+    headless: true
+  )
+end
+
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :cuprite, using: :chrome, screen_size: [ 1400, 1400 ]
+  driven_by :cuprite
 end
