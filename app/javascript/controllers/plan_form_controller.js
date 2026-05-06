0ba23d0b25f872835d0a72f42e78c868a2b8b7a1
John Alvin Ambalong <119690431+jambalong@users.noreply.github.com>
2026-05-05 20:10:27 -0700

    fix: clamp range check on blur instead of input

--- app/javascript/controllers/plan_form_controller.js
+++ app/javascript/controllers/plan_form_controller.js
@@ -1,39 +1,35 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
   clampLevel(event) {
-    const input = event.target
-    let value = parseInt(input.value)
+    const input = event.target;
+    let value = parseInt(input.value);
 
-    if (value > 90) input.value = 90
-    if (value < 1) input.value = 1
-
-    this.ensureValidRange()
+    if (value > 90) input.value = 90;
+    if (value < 1) input.value = 1;
   }
 
   clampAscensionRank(event) {
-    const input = event.target
-    let value = parseInt(input.value)
+    const input = event.target;
+    let value = parseInt(input.value);
 
-    if (value > 6) input.value = 6
-    if (value < 0) input.value = 0
-
-    this.ensureValidRange()
+    if (value > 6) input.value = 6;
+    if (value < 0) input.value = 0;
   }
 
   // Ensures Target is always >= Current
   ensureValidRange() {
-    const currentLvl = this.element.querySelector('input[name*="current_level"]')
-    const targetLvl = this.element.querySelector('input[name*="target_level"]')
-    const currentAsc = this.element.querySelector('input[name*="current_ascension_rank"]')
-    const targetAsc = this.element.querySelector('input[name*="target_ascension_rank"]')
+    const currentLvl = this.element.querySelector('input[name*="current_level"]');
+    const targetLvl = this.element.querySelector('input[name*="target_level"]');
+    const currentAsc = this.element.querySelector('input[name*="current_ascension_rank"]');
+    const targetAsc = this.element.querySelector('input[name*="target_ascension_rank"]');
 
     if (parseInt(targetLvl.value) < parseInt(currentLvl.value)) {
-      targetLvl.value = currentLvl.value
+      targetLvl.value = currentLvl.value;
     }
 
     if (parseInt(targetAsc.value) < parseInt(currentAsc.value)) {
-      targetAsc.value = currentAsc.value
+      targetAsc.value = currentAsc.value;
     }
   }
 }
