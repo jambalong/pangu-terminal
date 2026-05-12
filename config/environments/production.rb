ddeb4c0ab9436a7d280d47369f3b78f9caed8c20
John Alvin Ambalong <119690431+jambalong@users.noreply.github.com>
2026-05-11 20:07:47 -0700

    config: add Postmark SMTP for password reset emails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,13 +64,15 @@ Rails.application.configure do
   }
 
   # Specify outgoing SMTP server. Remember to add smtp/* credentials via bin/rails credentials:edit.
-  # config.action_mailer.smtp_settings = {
-  #   user_name: Rails.application.credentials.dig(:smtp, :user_name),
-  #   password: Rails.application.credentials.dig(:smtp, :password),
-  #   address: "smtp.example.com",
-  #   port: 587,
-  #   authentication: :plain
-  # }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    user_name: ENV["POSTMARK_API_TOKEN"],
+    password: ENV["POSTMARK_API_TOKEN"],
+    address: "smtp.postmarkapp.com",
+    port: 587,
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
